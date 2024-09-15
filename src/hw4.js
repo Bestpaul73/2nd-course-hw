3ac7b66c1a9df7ad6fb446597691e5a1d918e973
Paul Palkin <111692145+Bestpaul73@users.noreply.github.com>
2024-09-15 15:15:10 +0300

    hw4

---
+++ src/hw4.js
@@ -0,0 +1,31 @@
+// task1
+// const obj = {
+//   Коля: '200',
+//   Вася: '300',
+//   Петя: '400',
+// };
+
+// for (const key in obj) {
+//   console.log(`${key} — зарплата ${obj[key]} долларов`);
+// }
+
+// task2
+// let n = 1000;
+// let counter = 0;
+// do {
+//   n /= 2;
+//   counter++;
+// } while (n > 50);
+// console.log(n, counter);
+
+// let n = 1000;
+// let counter = 0;
+// for (; n > 50; n /= 2) {
+//   counter++;
+// }
+// console.log(n, counter);
+
+// task3
+// for (let firstFriday = 4; firstFriday <= 31; firstFriday+=7) {
+// console.log(`Сегодня пятница, ${firstFriday}-е число. Необходимо подготовить отчет.`);    
+// }
